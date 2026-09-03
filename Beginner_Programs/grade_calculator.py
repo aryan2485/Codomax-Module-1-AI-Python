13da38f98a4be6011e6ca1056a5aab0ea14b086a
aryan2485 <aryanmgamit.bscibm2024@iar.ac.in>
2026-09-03 15:24:46 +0530

    Create grade_calculator.py

---
+++ Beginner_Programs/grade_calculator.py
@@ -0,0 +1,18 @@
+# Student Grade Calculator
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Your Grade is:", grade)
