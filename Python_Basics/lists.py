c129ff8754c5e44704019d669889cab00bf65b4a
aryan2485 <aryanmgamit.bscibm2024@iar.ac.in>
2026-09-03 15:17:03 +0530

    Create lists.py

---
+++ Python_Basics/lists.py
@@ -0,0 +1,10 @@
+# Python Lists
+
+students = ["Aryan", "Rahul", "Priya", "Riya"]
+
+print("Students:", students)
+print("First Student:", students[0])
+
+students.append("Amit")
+
+print("Updated List:", students)
