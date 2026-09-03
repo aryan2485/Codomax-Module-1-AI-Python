50665569238cc7523ed6dba7babafbe78577d992
aryan2485 <aryanmgamit.bscibm2024@iar.ac.in>
2026-09-03 15:14:34 +0530

    Create data_types.py

---
+++ Python_Basics/data_types.py
@@ -0,0 +1,11 @@
+# Python Data Types
+
+name = "Aryan"          # String
+age = 21                # Integer
+percentage = 85.5       # Float
+is_student = True       # Boolean
+
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(is_student))
