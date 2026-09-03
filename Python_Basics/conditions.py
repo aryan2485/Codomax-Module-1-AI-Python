15759422c3565df0754ebf1e1df7482378e0b8d3
aryan2485 <aryanmgamit.bscibm2024@iar.ac.in>
2026-09-03 15:15:55 +0530

    Create conditions.py

---
+++ Python_Basics/conditions.py
@@ -0,0 +1,14 @@
+# Python Conditions
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 80:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Needs Improvement")
